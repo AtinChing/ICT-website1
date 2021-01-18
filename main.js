a7ab0907cb4657c7a226464173f2fc1977e4eb06
AtinChing <68199228+AtinChing@users.noreply.github.com>
2021-01-18 11:39:28 +0300

    original
    original

---
+++ main.js
@@ -0,0 +1,16 @@
+function onload(){
+	document.body.background = '#fbfab2';
+}
+
+function colourin(id){
+    id.style.fontWeight = "900";
+}
+function uncolourin(id){
+    id.style.fontWeight = "400";
+}
+function setCase(){
+    switch()
+}
+function draw(){
+
+}

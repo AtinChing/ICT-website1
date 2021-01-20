7d4bd7c21002601f84e4707ddc6a13f15db1eb05
AtinChing <68199228+AtinChing@users.noreply.github.com>
2021-01-20 18:41:04 +0300

    Changes
    Changes

--- main.js
+++ main.js
@@ -9,8 +9,10 @@ function uncolourin(id){
     id.style.fontWeight = "400";
 }
 function setCase(){
-    switch()
-}
+   }
 function draw(){
 
+}
+function setVideoPos(){
+	document.getElementById("video").style.objectPosition = "3000 30%"
 }

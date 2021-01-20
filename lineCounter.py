c0c31833efd95747f080912f98342f58ecb40757
AtinChing <68199228+AtinChing@users.noreply.github.com>
2021-01-20 18:51:46 +0300

    change in py file

--- lineCounter.py
+++ lineCounter.py
@@ -1,15 +1,16 @@
 import os
+import codecs
 num = 0
-directory_in_str = os.path.abspath(os.path.dirname(__file__)) # Directory
+directory_in_str = os.path.abspath(os.path.dirname(__file__)) + "\\" # Directory
+directory_in_str.replace("c:", "C:") # The first letter is turned from c to C
 directory =  os.fsencode(directory_in_str)
-print(directory_in_str)
-print(directory)
 for file in os.listdir(directory): # For every file in the directory
     filename = os.fsdecode(file)
-    print(file)
-    print(filename)
-    f = open(directory_in_str + filename, "r") 
+    f = codecs.open(directory_in_str + filename, "r", "UTF-8") 
     if filename.endswith(".js") or filename.endswith(".html'"): # If the file ends with cs
-        for line in f:
-            num += 1
+        if filename.endswith(".html") is False:
+            for line in f:
+                num += 1
+        elif filename.endswith(".html"):
+            num += len(f.readlines())
 print(num)

80306aa5c29961424292746675fd9b1e6898ba84
AtinChing <68199228+AtinChing@users.noreply.github.com>
2021-02-16 20:17:51 +0300

    Added new feature
    - Now a picture of the logo of the language the mouse is hovering over will appear on the screen.

--- main.js
+++ main.js
@@ -13,6 +13,27 @@ function setCase(){
 function draw(){
 
 }
+function changeLogo(logoLang){ // Logo lang is the language whose logo should be the picture.
+    var logoPic = document.getElementById('logo');
+    switch(logoLang){
+        case "C#":
+            logoPic.src = 'C%23Logo.png';
+
+            break;
+        case "Java":
+            logoPic.src = 'JavaLogo.png';
+            break;
+        case "Cpp":
+            logoPic.src = 'CppLogo.png';
+            break;
+        case "Python":
+            logoPic.src = 'PythonLogo.png';
+            break;
+        case "TypeScript":
+            logoPic.src = 'TSLogo.png';
+            break;
+    }
+}
 function setVideoPos(){
 	document.getElementById("video").style.objectPosition = "3000 30%"
 }
